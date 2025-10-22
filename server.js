e63c6cf998a678b96a20d44e3e54aca618ca8166
Paul Yusuf <paulyusuf2000@gmail.com>
2025-10-22 02:30:06 +0100

    Update for Deployment

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./conf.env" });
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+const PORTS = process.env.PORTS || 3000;
 const NODE_ENV = process.env.NODE_ENV || "development";
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const server = app.listen(PORTS, () => {
+  console.log(`Server is running on port ${PORTS}`);
   console.log("Running Environment:", NODE_ENV);
 });
